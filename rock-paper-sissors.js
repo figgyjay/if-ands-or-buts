043749c1788aa68ba016682b341383ab120ccfbb
figgyjay <79807654+figgyjay@users.noreply.github.com>
2022-06-07 19:23:36 -0500

    Add files via upload

--- rock-paper-sissors.js
+++ rock-paper-sissors.js
@@ -9,32 +9,32 @@ let Player2 = (prompt("Player 1: Pick Paper, Rock or Scissors "))
 if (Player1 === "rock" && Player2 === "scissors"){
     console.log("Player 1 wins! ")
 }
-if (Player1 === "scissors" && Player2 === "paper"){
+else if (Player1 === "scissors" && Player2 === "paper"){
     console.log("Player 1 wins! ")
 }
-if (Player1 === "paper" && Player2 === "rock"){
+else if (Player1 === "paper" && Player2 === "rock"){
     console.log("Player 1 wins! ")
 }
 
 //if player two wins:
 
-if (Player2 === "rock" && Player1 === "scissors"){
+else if (Player2 === "rock" && Player1 === "scissors"){
     console.log("Player 1 wins! ")
 }
-if (Player2 === "scissors" && Player1 === "paper"){
+else if (Player2 === "scissors" && Player1 === "paper"){
     console.log("Player 1 wins! ")
 }
-if (Player2 === "paper" && Player1 === "rock"){
+else if (Player2 === "paper" && Player1 === "rock"){
     console.log("Player 1 wins! ")
 }
 
 // if there is a tie:
-if (Player1 === "rock" && Player2 === "rock"){
+else if (Player1 === "rock" && Player2 === "rock"){
     console.log(" Tie! ")
 }
-if (Player1 === "scissors" && Player2 === "scissors"){
+else if (Player1 === "scissors" && Player2 === "scissors"){
     console.log(" Tie! ")
 }
-if (Player1 === "paper" && Player2 === "paper"){
+else if (Player1 === "paper" && Player2 === "paper"){
     console.log(" Tie! ")
 }
